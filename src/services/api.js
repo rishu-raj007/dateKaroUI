e27c1bd97f9186e9c322ea5914c0a7afc7f45cad
Rishu Raj <154724023+rishu-raj007@users.noreply.github.com>
2026-03-03 01:20:10 +0530

    Change API URL to production server

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5050/api';
+const API_URL = 'https://datekaroserver.onrender.com';
 
 const api = axios.create({
     baseURL: API_URL,
