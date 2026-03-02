365fe8654787ed4e977ee4f7b1b631b2941d0093
Rishu Raj <154724023+rishu-raj007@users.noreply.github.com>
2026-03-03 01:37:45 +0530

    Configure server and preview settings in Vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173,
+    allowedHosts: [
+      'datekaroui.onrender.com'
+    ]
+  }
 })
