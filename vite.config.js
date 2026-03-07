73cc68ec79da8de89fdbf07a17a2a9ff950760e3
Rishu Raj <154724023+rishu-raj007@users.noreply.github.com>
2026-03-07 14:55:40 +0530

    Add React plugin to Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  plugins: [react()], // <--- ADD THIS LINE HERE
   server: {
     host: '0.0.0.0',
     port: process.env.PORT || 5173
